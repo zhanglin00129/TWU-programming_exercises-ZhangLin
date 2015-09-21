47868c34c4050c09a836911a62d97eacd34968e0
Zhang Lin <lin.zhang@rea-group.com>
2015-09-22 00:41:50 +0800

    Draw a horizontal line

--- src/main/java/com/twu/exercises/triangleExercises/Triangle.java
+++ src/main/java/com/twu/exercises/triangleExercises/Triangle.java
@@ -4,4 +4,12 @@ public class Triangle {
     public String execute() {
         return "*";
     }
+
+    public String horizontal(int num) {
+        String result = "";
+        for (int j = 0; j < num; j++) {
+            result += "*";
+        }
+        return result;
+    }
 }
--- src/main/test/com/twu/exercises/triangleExercises/triangleTest.java
+++ src/main/test/com/twu/exercises/triangleExercises/triangleTest.java
@@ -1,5 +1,6 @@
 package com.twu.exercises.triangleExercises;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
@@ -8,8 +9,18 @@ import static org.junit.Assert.assertThat;
 public class triangleTest {
     private Triangle triangle;
 
+    @Before
+    public void setUp() throws Exception {
+        triangle = new Triangle();
+    }
+
     @Test
     public void testEasiestExercise() throws Exception {
         assertThat(triangle.execute(), is("*"));
+    }
+
+    @Test
+    public void testHorizontalLine() throws Exception {
+        assertThat(triangle.horizontal(8), is("********"));
     }
 }
