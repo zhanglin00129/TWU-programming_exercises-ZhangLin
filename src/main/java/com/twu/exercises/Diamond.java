f9f2eba16557b8ee8a76d521ffa83868d8b8acb7
Zhang Lin <lin.zhang@rea-group.com>
2015-09-23 01:07:47 +0800

    refactor code && draw a diamond with name

--- src/main/java/com/twu/exercises/Diamond.java
+++ src/main/java/com/twu/exercises/Diamond.java
@@ -3,10 +3,12 @@ package com.twu.exercises;
 import org.apache.commons.lang3.StringUtils;
 
 public class Diamond {
-    public String isosceles(int num) {
-        String result = "";
 
-        for (int i = 0; i < num; i++) {
+    public static final String MY_NAME = "Lin\n";
+
+    private String upTriangle(int num) {
+        String result = "";
+        for (int i = 0; i < num - 1; i++) {
             result += StringUtils.repeat(" ", (num - i - 1));
             result += StringUtils.repeat("*", (2 * i + 1));
             result += "\n";
@@ -14,13 +16,27 @@ public class Diamond {
         return result;
     }
 
-    public String rhombus(int num) {
-        String result = isosceles(num);
+    private String downTriangle(int num) {
+        String result = "";
         for (int i = num; i < 2 * num - 1; i++) {
             result += StringUtils.repeat(" ", (1 + i - num));
             result += StringUtils.repeat("*", (4 * num - 2 * i - 3));
             result += "\n";
         }
         return result;
+    }
+
+    public String isosceles(int num) {
+        return upTriangle(num) +
+                StringUtils.repeat("*", (2 * num - 1)) +
+                "\n";
+    }
+
+    public String rhombus(int num) {
+        return isosceles(num) + downTriangle(num);
+    }
+
+    public String rhombusName(int num) {
+        return upTriangle(num) + MY_NAME + downTriangle(num);
     }
 }
--- src/main/test/com/twu/exercises/DiamondTest.java
+++ src/main/test/com/twu/exercises/DiamondTest.java
@@ -26,4 +26,10 @@ public class DiamondTest {
         String expect = "  *\n ***\n*****\n ***\n  *\n";
         assertThat(diamond.rhombus(3), is(expect));
     }
+
+    @Test
+    public void testDiamendWithName() throws Exception {
+        String expect = "  *\n ***\nLin\n ***\n  *\n";
+        assertThat(diamond.rhombusName(3), is(expect));
+    }
 }
